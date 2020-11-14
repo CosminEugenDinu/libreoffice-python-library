8ec77f56eb83c2debc91e484a93a7964f6771e96
CosminEugen <cosmineugendinu@gmail.com>
2020-11-15 01:38:18 +0200

    feat: Calc creation and save example

--- examples/new_calc.py
+++ examples/new_calc.py
@@ -4,7 +4,20 @@ sys.path.append('.')
 from src import document
 
 calc = document.Calc()
-print(dir(calc))
 
+sheets = calc.get_sheets()
+sheet_1 = sheets[0]
+cell_1 = sheet_1.getCellByPosition(1,1)
+value = cell_1.setString('Hello World!')
+
+calc.save('./examples/saved/somefile.ods')
+calc.save('./examples/saved/somefile.jpg', 'jpg')
+# calc.save('./examples/somefile.svg', 'svg')
+calc.save('./examples/saved/somefile.pdf', 'pdf')
+calc.save('./examples/saved/somefile.png', 'png')
+calc.save('./examples/saved/somefile.xls', 'xls')
+calc.save('./examples/saved/somefile.xlsx', 'xlsx')
+
+calc.desktop.terminate()
 
 
