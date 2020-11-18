0839dbe78c361487a64c816549acb6976ea8d6e1
CosminEugen <cosmineugendinu@gmail.com>
2020-11-18 13:01:30 +0200

    feat: Implemented Calc: Range, Sheet

--- test/test.py
+++ test/test.py
@@ -1,1 +1,11 @@
 # testing
+import sys
+sys.path.append('.')
+from libreoffice_py import document
+
+calc = document.Calc()
+sheet = calc.get_sheets()[0]
+range = sheet.get_range(0, 0, 0, 0)
+data = range.set_data([[1]])
+calc.save('./test/testfile.ods')
+
